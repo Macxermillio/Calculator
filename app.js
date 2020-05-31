b0886e53dff0103e2af0be58afe679bfb33ddae1
Sandy Masia <macxermillio@gmail.com>
2020-05-31 12:28:38 +0200

    ""

---
+++ app.js
@@ -0,0 +1,10 @@
+class Calculator
+
+
+const numberButtons = document.querySelectorAll("[data-number]")
+const operationButtons = document.querySelectorAll("[data-operation]")
+const equalsButton = document.querySelector("[data-equals]")
+const allclearButton = document.querySelector("[data-all-clear]")
+const deleteButton = document.querySelector("[data-delete]")
+const previousOperand = document.querySelector("[data-previous-operand]")
+const currentOperand = document.querySelector("[data-current-operand]")
