57817045b5ad930e814dce740a389daf60fa4b49
Sandy Masia <macxermillio@gmail.com>
2020-05-31 15:56:17 +0200

    "work for the day"

--- app.js
+++ app.js
@@ -1,4 +1,37 @@
-class Calculator
+class Calculator{
+    
+    constructor(currentOperandText,previousOperandText){
+        this.currentOperandText = currentOperandText
+        this.previousOperandText = previousOperandText
+        this.clear()
+    }
+
+    delete(){
+
+    }
+
+    clear(){
+        this.operatrion = undefined
+        this.currentOperand = ''
+        this.previousOperand = ''
+    }
+
+    addNumber(number){
+        this.currentOperand = this.currentOperand.toString() + number.toString();
+    }
+
+    chosenOperator(operatrion){
+
+    }
+
+    compute(){
+
+    }
+    
+    update(){
+        this.currentOperandText.innerText = this.currentOperand
+    }
+}
 
 
 const numberButtons = document.querySelectorAll("[data-number]")
@@ -6,5 +39,15 @@ const operationButtons = document.querySelectorAll("[data-operation]")
 const equalsButton = document.querySelector("[data-equals]")
 const allclearButton = document.querySelector("[data-all-clear]")
 const deleteButton = document.querySelector("[data-delete]")
-const previousOperand = document.querySelector("[data-previous-operand]")
-const currentOperand = document.querySelector("[data-current-operand]")
+const previousOperandText = document.querySelector("[data-previous-operand]")
+const currentOperandText = document.querySelector("[data-current-operand]")
+
+
+const calculator = new Calculator(currentOperandText,previousOperandText)
+
+numberButtons.forEach( button => {
+    button.addEventListener("click",() => {
+        calculator.addNumber(button.innerHTML)
+        calculator.update()
+    })
+})
